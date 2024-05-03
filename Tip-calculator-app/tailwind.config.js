522ac74d5ef9f9446562e32616d29cb4b9b522d4
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-05-03 11:19:49 +0200

    Add html structure with mobile tailwind classes

--- Tip-calculator-app/tailwind.config.js
+++ Tip-calculator-app/tailwind.config.js
@@ -3,8 +3,23 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      fontFamily: {
+        body: ['Space Mono'],
+      },
       colors: {
-        'light-green': '#C5E4E7',
+        'strong-cyan': '#26C2AE',
+        'very-dark-cyan': '#00474B',
+        'dark-grayish-cyan': '#5e7a7d',
+        'grayish-cyan': '#7F9D9F',
+        'light-grayish-cyan': '#c5e4e7',
+        'very-light-grayish-cyan': '#F3F9FA',
+      },
+      lineHeight: {
+        12: '3rem',
+      },
+      backgroundImage: {
+        'dollar-icon': "url('../../assets/images/icon-dollar.svg')",
+        'user-icon': "url('../../assets/images/icon-person.svg')",
       },
     },
   },
