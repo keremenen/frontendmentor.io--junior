87d90ca6a46b6224e1dec001714f3679037a5857
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-05-03 08:16:04 +0200

    Initial commit

---
+++ Tip-calculator-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'light-green': '#C5E4E7',
+      },
+    },
+  },
+  plugins: [],
+}
